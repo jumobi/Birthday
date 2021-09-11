c8e121ce5a0dcf2352dd79b066357910e0aff6d0
jumobi <jodusole@gmail.com>
2021-09-11 17:32:27 +0100

    first commit

---
+++ function.js
@@ -0,0 +1,11 @@
+alert("TOUCH THE CAKE 🎂 AND ENJOY THE PARTY 🎶🎉🎊");
+window.addEventListener('click', function() {
+    var audio = document.getElementById("HBD");
+    audio.play();
+});
+
+alert("TOUCH THE CAKE 🎂 AND ENJOY THE PARTY 🎶🎉🎊");
+window.addEventListener('click', function() {
+    var audio = document.getElementById("HBD");
+    audio.play();
+});
